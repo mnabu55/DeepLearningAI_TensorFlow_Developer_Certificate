97c2dfe77aab7289a0eb8cea2b49bf2509e89901
Mark Kiyota <agmk4655@gmail.com>
2022-11-05 16:10:16 +0900

    Update c2w3lab1_transfer_learning.py

--- work/classification/c2w3lab1_transfer_learning.py
+++ work/classification/c2w3lab1_transfer_learning.py
@@ -47,7 +47,7 @@ x = layers.Dense(1024, activation='relu')(x)
 # Add a dropout rate of 0.2
 x = layers.Dropout(0.2)(x)
 # Add a final sigmoid layer for classification
-x = layers.Dense  (1, activation='sigmoid')(x)
+x = layers.Dense(1, activation='sigmoid')(x)
 
 # Append the dense network to the base model
 model = Model(pre_trained_model.input, x)
