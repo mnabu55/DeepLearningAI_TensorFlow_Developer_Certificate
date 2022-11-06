21ed140029a1cfc9d7edf32a87a1bc77a4a7e2b7
Manabu Kiyota <agmk4655@gmail.com>
2022-11-06 19:19:54 +0900

    Update c3w1lab3_scarcasm_dataset.py

--- work/nlp/c3w1lab3_scarcasm_dataset.py
+++ work/nlp/c3w1lab3_scarcasm_dataset.py
@@ -11,3 +11,35 @@ with open("./sarcasm.json", "r") as f:
 
 print(datastore[0])
 print(datastore[20000])
+
+sentences = []
+labels = []
+urls = []
+
+for item in datastore:
+    sentences.append(item["headline"])
+    labels.append(item["is_sarcastic"])
+    urls.append(item["article_link"])
+
+print("len(sentences): ", len(sentences))
+
+# create Tokenizer
+tokenizer = Tokenizer(oov_token="<OOV>")
+
+# Generate the word index dictionary
+tokenizer.fit_on_texts(sentences)
+
+word_index = tokenizer.word_index
+print(f"number of words in word_index: {len(word_index)}\n")
+
+# generate padded sequences
+sequences = tokenizer.texts_to_sequences(sentences)
+padded = pad_sequences(sequences, padding="post")
+
+# print sample headlie
+index = 2
+print(f"sample headline: {sentences[index]}")
+print(f"sequence: {sequences[index]}")
+print(f"padded sequence: {padded[index]}")
+
+
