84c381e7f9455c604d07e4885b0ec861f8ac95e0
Mark Kiyota <agmk4655@gmail.com>
2022-11-08 15:40:54 +0900

    reduce batchsize
    batchsize256 cannot work propery.
    so value set to lower 128.

--- work/nlp/c3w3lab1_single_layer_LSTM.py
+++ work/nlp/c3w3lab1_single_layer_LSTM.py
@@ -6,10 +6,10 @@ dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True, as_supervis
 tokenizer = info.features['text'].encoder
 
 BUFFER_SIZE = 10000
-BATCH_SIZE = 256
+BATCH_SIZE = 128
 
 # Get the train and test splits
-train_data, test_data = dataset['train'], dataset['test'],
+train_data, test_data = dataset['train'], dataset['test']
 
 # Shuffle the training data
 train_dataset = train_data.shuffle(BUFFER_SIZE)
