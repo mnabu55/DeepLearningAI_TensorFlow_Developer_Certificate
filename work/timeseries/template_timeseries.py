04bcc23a21721733d0788837249a92d5b2aab5da
Mark Kiyota <agmk4655@gmail.com>
2022-11-13 15:42:33 +0900

    Update template_timeseries.py

--- work/timeseries/template_timeseries.py
+++ work/timeseries/template_timeseries.py
@@ -1,26 +1,218 @@
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
-# Generate the data. data shape: list ex) [1, 2, 3, ...]
-dataset = tf.data.Dataset.range(10)
 
-# Window. data shape: Window Dataset
-dataset = dataset.window(5, shift=1, drop_remainder=True)
+def plot_series(time, series, format="-", start=0, end=None):
+    """
+    Visualizes time series data
 
-# Flatten the windows. data shape: ex) [1 2 3 ...]
-dataset = dataset.flat_map(lambda window: window.batch(5))
+    Args:
+      time (array of int) - contains the time steps
+      series (array of int) - contains the measurements for each time step
+      format - line style when plotting the graph
+      label - tag for the line
+      start - first time step to plot
+      end - last time step to plot
+    """
 
-# Create tuples with features and labels
-dataset = dataset.map(lambda window: (window[:-1], window[-1]))
+    # Setup dimensions of the graph figure
+    plt.figure(figsize=(10, 6))
 
-# Shuffle the window
-# buffer size should be more than the total number of windows
-dataset = dataset.shuffle(buffer_size=10)
+    if type(series) is tuple:
 
-# Create batches of windows
-dataset = dataset.batch(2).prefetch(1)
+        for series_num in series:
+            # Plot the time series data
+            plt.plot(time[start:end], series_num[start:end], format)
 
-for x, y in dataset:
-    print("x = ", x.numpy())
-    print("y = ", y.numpy())
-    print()
+    else:
+        # Plot the time series data
+        plt.plot(time[start:end], series[start:end], format)
+
+    # Label the x-axis
+    plt.xlabel("Time")
+
+    # Label the y-axis
+    plt.ylabel("Value")
+
+    # Overlay a grid on the graph
+    plt.grid(True)
+
+    # Draw the graph on screen
+    plt.show()
+
+
+def trend(time, slope=0):
+    """
+    Generates synthetic data that follows a straight line given a slope value.
+
+    Args:
+      time (array of int) - contains the time steps
+      slope (float) - determines the direction and steepness of the line
+
+    Returns:
+      series (array of float) - measurements that follow a straight line
+    """
+
+    # Compute the linear series given the slope
+    series = slope * time
+
+    return series
+
+
+def seasonal_pattern(season_time):
+    """
+    Just an arbitrary pattern, you can change it if you wish
+
+    Args:
+      season_time (array of float) - contains the measurements per time step
+
+    Returns:
+      data_pattern (array of float) -  contains revised measurement values according
+                                  to the defined pattern
+    """
+
+    # Generate the values using an arbitrary pattern
+    data_pattern = np.where(season_time < 0.4,
+                            np.cos(season_time * 2 * np.pi),
+                            1 / np.exp(3 * season_time))
+
+    return data_pattern
+
+
+def seasonality(time, period, amplitude=1, phase=0):
+    """
+    Repeats the same pattern at each period
+
+    Args:
+      time (array of int) - contains the time steps
+      period (int) - number of time steps before the pattern repeats
+      amplitude (int) - peak measured value in a period
+      phase (int) - number of time steps to shift the measured values
+
+    Returns:
+      data_pattern (array of float) - seasonal data scaled by the defined amplitude
+    """
+
+    # Define the measured values per period
+    season_time = ((time + phase) % period) / period
+
+    # Generates the seasonal data scaled by the defined amplitude
+    data_pattern = amplitude * seasonal_pattern(season_time)
+
+    return data_pattern
+
+
+def noise(time, noise_level=1, seed=None):
+    """Generates a normally distributed noisy signal
+
+    Args:
+      time (array of int) - contains the time steps
+      noise_level (float) - scaling factor for the generated signal
+      seed (int) - number generator seed for repeatability
+
+    Returns:
+      noise (array of float) - the noisy signal
+    """
+
+    # Initialize the random number generator
+    rnd = np.random.RandomState(seed)
+
+    # Generate a random number for each time step and scale by the noise level
+    noise = rnd.randn(len(time)) * noise_level
+
+    return noise
+
+
+
+# Generate the data
+
+# Parameters
+time = np.arange(4 * 365 + 1, dtype="float32")
+baseline = 10
+amplitude = 40
+slope = 0.05
+noise_level = 5
+
+# Create the series
+series = baseline + trend(time, slope) + seasonality(time, period=365, amplitude=amplitude)
+
+# Update with noise
+series += noise(time, noise_level, seed=42)
+
+# Plot the results
+plot_series(time, series)
+
+
+# Split the dataset
+
+# Define the split time
+split_time = 1000
+
+# Get the train set
+time_train = time[:split_time]
+x_train = series[:split_time]
+
+# Get the validation set
+time_valid = time[split_time:]
+x_valid = series[split_time:]
+
+
+# Parameters
+window_size = 20
+batch_size = 32
+shuffle_buffer_size = 1000
+
+
+def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
+    # Generate a tf dataset from the series values
+    dataset = tf.data.Dataset.from_tensor_slices(series)
+
+    # window
+    dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
+
+    # Flatten [1, 2, 3,...] -> [1 2 3 ...]
+    dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
+
+    # Create tuples
+    dataset = dataset.map(lambda window: (window[:-1], window[-1]))
+
+    # shuffle the window
+    dataset = dataset.shuffle(shuffle_buffer)
+
+    # create batches of windows
+    dataset = dataset.batch(batch_size).prefetch(1)
+
+    return dataset
+
+
+dataset = windowed_dataset(x_train, window_size, batch_size, shuffle_buffer_size)
+
+# Print properties of a single batch
+for windows in dataset.take(1):
+  print(f'data type: {type(windows)}')
+  print(f'number of elements in the tuple: {len(windows)}')
+  print(f'shape of first element: {windows[0].shape}')
+  print(f'shape of second element: {windows[1].shape}')
+
+
+
+# create a model
+IO = tf.keras.layers.Dense(1, input_shape=[window_size])
+model = tf.keras.models.Sequential([
+    IO
+])
+
+print(f"Layer weights: \n {IO.get_weights()}\n")
+
+# print summary
+model.summary()
+
+# compile the model
+model.compile(loss="mse",
+              optimizer=tf.keras.optimizers.SGD(learning_rate=1e-6, momentum=0.9))
+
+# train the model
+model.fit(dataset, epochs=100)
+
+print(f"Layer weights: \n {IO.get_weights()}\n")
