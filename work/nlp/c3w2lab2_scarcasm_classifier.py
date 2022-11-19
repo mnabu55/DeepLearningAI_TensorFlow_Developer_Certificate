ab3b99338f5aadca227bcea997e4a926d2c2c6a6
Mark Kiyota <agmk4655@gmail.com>
2022-11-19 15:41:16 +0900

    add initial

---
+++ work/nlp/c3w2lab2_scarcasm_classifier.py
@@ -0,0 +1,100 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import requests
+import json
+import numpy as np
+
+
+response = requests.get("https://storage.googleapis.com/tensorflow-1-public/course3/sarcasm.json")
+open("sarcasm.json", "wb").write(response.content)
+
+with open("./sarcasm.json", "r") as f:
+    datastore = json.load(f)
+
+print(datastore[0])
+print(datastore[20000])
+
+sentences = []
+labels = []
+urls = []
+
+for item in datastore:
+    sentences.append(item["headline"])
+    labels.append(item["is_sarcastic"])
+    urls.append(item["article_link"])
+
+print("len(sentences): ", len(sentences))
+
+
+# Hyperparameters
+training_size = 20000
+vocab_size = 10000
+max_length = 32
+embedding_dim = 16
+trunc_type = "post"
+padding_type = "post"
+oov_token = "<OOV>"
+
+
+# split training and test data
+training_sentences = sentences[:training_size]
+testing_sentences = sentences[training_size:]
+training_labels = labels[:training_size]
+testing_labels = labels[training_size:]
+
+# define tokenizer
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_token)
+
+tokenizer.fit_on_texts(training_sentences)
+word_index = tokenizer.word_index
+
+# generate padded sequences
+training_sequences = tokenizer.texts_to_sequences(training_sentences)
+training_padded = pad_sequences(training_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(testing_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+# training_padded = np.array(training_padded)
+# testing_padded = np.array(testing_padded)
+training_labels = np.array(training_labels)
+testing_labels = np.array(testing_labels)
+
+# build the model
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Dense(24, activation="relu"),
+    tf.keras.layers.Dense(1, activation="sigmoid")
+])
+
+model.summary()
+
+# compile the model
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=["accuracy"])
+
+# train the model
+num_epochs = 30
+history = model.fit(training_padded, training_labels, epochs=num_epochs,
+                    validation_data=(testing_padded, testing_labels),
+                    verbose=2)
+
+import matplotlib.pyplot as plt
+
+
+# Plot utility
+def plot_graphs(history, string):
+    plt.plot(history.history[string])
+    plt.plot(history.history['val_' + string])
+    plt.xlabel("Epochs")
+    plt.ylabel(string)
+    plt.legend([string, 'val_' + string])
+    plt.show()
+
+
+# Plot the accuracy and loss
+plot_graphs(history, "accuracy")
+plot_graphs(history, "loss")
