2cb4fe91530c864031bca781b505f4e3b1f21a45
Mark Kiyota <agmk4655@gmail.com>
2022-11-23 16:17:54 +0900

    mse: 442.61, mae: 14.74
    window_size: 30
    batch_size: 32
    dense 30
    dense 10
    lr: 4e-5

---
+++ work/timeseries/timeseries_sunspot.py
@@ -0,0 +1,340 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import csv
+import requests
+from dataclasses import dataclass
+
+
+def plot_series(time, series, format="-", start=0, end=None):
+    """
+    Visualizes time series data
+
+    Args:
+      time (array of int) - contains the time steps
+      series (array of int) - contains the measurements for each time step
+      format - line style when plotting the graph
+      label - tag for the line
+      start - first time step to plot
+      end - last time step to plot
+    """
+
+    # Setup dimensions of the graph figure
+    plt.figure(figsize=(10, 6))
+
+    if type(series) is tuple:
+
+        for series_num in series:
+            # Plot the time series data
+            plt.plot(time[start:end], series_num[start:end], format)
+
+    else:
+        # Plot the time series data
+        plt.plot(time[start:end], series[start:end], format)
+
+    # Label the x-axis
+    plt.xlabel("Time")
+
+    # Label the y-axis
+    plt.ylabel("Value")
+
+    # Overlay a grid on the graph
+    plt.grid(True)
+
+    # Draw the graph on screen
+    plt.show()
+
+
+def trend(time, slope=0):
+    """
+    Generates synthetic data that follows a straight line given a slope value.
+
+    Args:
+      time (array of int) - contains the time steps
+      slope (float) - determines the direction and steepness of the line
+
+    Returns:
+      series (array of float) - measurements that follow a straight line
+    """
+
+    # Compute the linear series given the slope
+    series = slope * time
+
+    return series
+
+
+def seasonal_pattern(season_time):
+    """
+    Just an arbitrary pattern, you can change it if you wish
+
+    Args:
+      season_time (array of float) - contains the measurements per time step
+
+    Returns:
+      data_pattern (array of float) -  contains revised measurement values according
+                                  to the defined pattern
+    """
+
+    # Generate the values using an arbitrary pattern
+    data_pattern = np.where(season_time < 0.4,
+                            np.cos(season_time * 2 * np.pi),
+                            1 / np.exp(3 * season_time))
+
+    return data_pattern
+
+
+def seasonality(time, period, amplitude=1, phase=0):
+    """
+    Repeats the same pattern at each period
+
+    Args:
+      time (array of int) - contains the time steps
+      period (int) - number of time steps before the pattern repeats
+      amplitude (int) - peak measured value in a period
+      phase (int) - number of time steps to shift the measured values
+
+    Returns:
+      data_pattern (array of float) - seasonal data scaled by the defined amplitude
+    """
+
+    # Define the measured values per period
+    season_time = ((time + phase) % period) / period
+
+    # Generates the seasonal data scaled by the defined amplitude
+    data_pattern = amplitude * seasonal_pattern(season_time)
+
+    return data_pattern
+
+
+def noise(time, noise_level=1, seed=None):
+    """Generates a normally distributed noisy signal
+
+    Args:
+      time (array of int) - contains the time steps
+      noise_level (float) - scaling factor for the generated signal
+      seed (int) - number generator seed for repeatability
+
+    Returns:
+      noise (array of float) - the noisy signal
+    """
+
+    # Initialize the random number generator
+    rnd = np.random.RandomState(seed)
+
+    # Generate a random number for each time step and scale by the noise level
+    noise = rnd.randn(len(time)) * noise_level
+
+    return noise
+
+
+def generate_time_series():
+    # The time dimension or the x-coordinate of the time series
+    time = np.arange(4 * 365 + 1, dtype="float32")
+
+    # Initial series is just a straight line with a y-intercept
+    y_intercept = 10
+    slope = 0.005
+    series = trend(time, slope) + y_intercept
+
+    # Adding seasonality
+    amplitude = 50
+    series += seasonality(time, period=365, amplitude=amplitude)
+
+    # Adding some noise
+    noise_level = 3
+    series += noise(time, noise_level, seed=51)
+
+    return time, series
+
+
+def parse_data_from_file(filename):
+    """
+    :param filename:
+    :return:
+        times - time steps
+        series - measurements
+    """
+    times = []
+    series = []
+
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+
+        # skip the header
+        next(reader)
+
+        for row in reader:
+            times.append(int(row[0]))
+            series.append(float(row[2]))
+
+    times = np.array(times)
+    series = np.array(series)
+
+    return times, series
+
+
+file_url = "https://storage.googleapis.com/tensorflow-1-public/course4/Sunspots.csv"
+response = requests.get(file_url)
+open("Sunspots.csv", "wb").write(response.content)
+
+
+@dataclass
+class G:
+    DATA_CSV = "./Sunspots.csv"
+    times, series = parse_data_from_file(DATA_CSV)
+    TIME = np.array(times)
+    SERIES = np.array(series)
+    SPLIT_TIME = 3000
+    WINDOW_SIZE = 30
+    BATCH_SIZE = 32
+    SHUFFLE_BUFFER_SIZE = 1000
+
+
+plot_series(G.TIME, G.SERIES)
+
+
+# Split the dataset
+def train_val_split(time, series, time_step=G.SPLIT_TIME):
+
+    time_train = time[:time_step]
+    series_train = series[:time_step]
+    time_valid = time[time_step:]
+    series_valid = series[time_step:]
+
+    return time_train, series_train, time_valid, series_valid
+
+
+# Split the dataset
+time_train, series_train, time_valid, series_valid = train_val_split(G.TIME, G.SERIES)
+
+
+def windowed_dataset(series, window_size=G.WINDOW_SIZE, batch_size=G.BATCH_SIZE, shuffle_buffer=G.SHUFFLE_BUFFER_SIZE):
+    ds = tf.data.Dataset.from_tensor_slices(series)
+    ds = ds.window(window_size + 1, shift=1, drop_remainder=True)
+    ds = ds.flat_map(lambda w: w.batch(window_size + 1))
+    ds = ds.shuffle(shuffle_buffer)
+    ds = ds.map(lambda w: (w[:-1], w[-1]))
+    ds = ds.batch(batch_size).prefetch(1)
+    return ds
+
+
+# Apply the transformation to the training set
+train_set = windowed_dataset(series_train, window_size=G.WINDOW_SIZE, batch_size=G.BATCH_SIZE, shuffle_buffer=G.SHUFFLE_BUFFER_SIZE)
+
+
+def create_uncompiled_model():
+    ### START CODE HERE
+
+    # model = tf.keras.models.Sequential([
+    #     tf.keras.layers.Conv1D(filters=64, kernel_size=3,
+    #                            strides=1, padding="causal",
+    #                            activation="relu",
+    #                            input_shape=[G.WINDOW_SIZE, 1]),
+    #     tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True)),
+    #     tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
+    #     tf.keras.layers.Dense(1)
+    # ])
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Dense(30, input_shape=[G.WINDOW_SIZE], activation="relu"),
+        tf.keras.layers.Dense(10, activation="relu"),
+        tf.keras.layers.Dense(1)
+    ])
+    ### END CODE HERE
+
+    return model
+
+
+# Test your uncompiled model
+uncompiled_model = create_uncompiled_model()
+
+try:
+    uncompiled_model.predict(train_set)
+except:
+    print("Your current architecture is incompatible with the windowed dataset, try adjusting it.")
+else:
+    print("Your current architecture is compatible with the windowed dataset! :)")
+
+
+def adjust_learning_rate(dataset):
+    model = create_uncompiled_model()
+
+    lr_schedule = tf.keras.callbacks.LearningRateScheduler(
+        lambda epoch: 1e-8 * 10 ** (epoch / 20))
+
+    ### START CODE HERE
+
+    # Select your optimizer
+    optimizer = tf.keras.optimizers.SGD(momentum=0.9)
+
+    # Compile the model passing in the appropriate loss
+    model.compile(loss=tf.keras.losses.Huber(),
+                  optimizer=optimizer,
+                  metrics=["mae"])
+
+    ### END CODE HERE
+
+    history = model.fit(dataset, epochs=100, callbacks=[lr_schedule])
+
+    return history
+
+
+# Run the training with dynamic LR
+lr_history = adjust_learning_rate(train_set)
+
+plt.semilogx(lr_history.history["lr"], lr_history.history["loss"])
+plt.axis([1e-8, 1e-3, 0, 100])
+plt.show()
+
+
+def create_model():
+    model = create_uncompiled_model()
+
+    ### START CODE HERE
+    learning_rate = 4e-5
+    optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate,
+                                        momentum=0.9)
+    model.compile(loss=tf.keras.losses.Huber(),
+                  optimizer=optimizer,
+                  metrics=["mae"])
+    ### END CODE HERE
+
+    return model
+
+
+# Save an instance of the model
+model = create_model()
+
+# Train it
+history = model.fit(train_set, epochs=50)
+
+
+def compute_metrics(true_series, forecast):
+    mse = tf.keras.metrics.mean_squared_error(true_series, forecast).numpy()
+    mae = tf.keras.metrics.mean_absolute_error(true_series, forecast).numpy()
+
+    return mse, mae
+
+
+def model_forecast(model, series, window_size):
+    ds = tf.data.Dataset.from_tensor_slices(series)
+    ds = ds.window(window_size, shift=1, drop_remainder=True)
+    ds = ds.flat_map(lambda w: w.batch(window_size))
+    ds = ds.batch(32).prefetch(1)
+    forecast = model.predict(ds)
+    return forecast
+
+
+# Compute the forecast for all the series
+forecast = model_forecast(model, G.SERIES, G.WINDOW_SIZE).squeeze()
+
+# Slice the forecast to get only the predictions for the validation set
+forecast = forecast[G.SPLIT_TIME - G.WINDOW_SIZE:-1]
+
+# Plot the forecast
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, (series_valid, forecast))
+#plot_series(time_valid, forecast)
+
+
+mse, mae = compute_metrics(series_valid, forecast)
+print(f"mse: {mse:.2f}, mae: {mae:.2f} for forecast")
